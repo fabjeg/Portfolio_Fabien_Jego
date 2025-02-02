d2295f0340fd9dcb4b60cc3bb4d9d24e7aa80805
Jego <fabien.jego.p@gmail.com>
2025-02-02 01:42:55 +0100

    modify footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,22 @@
-import { ButtonScrollTop } from '../buttons/button-scrool-top'
-import './style.css'
+import { ButtonScrollTop } from '../buttons/button-scrool-top';
+import './style.css';
 
 export function Footer() {
+    const logos = [
+        'fa fa-github',
+        'fa fa-linkedin-square'
+    ];
+
     return (
         <div className="container-footer">
-            <ButtonScrollTop/>
+            <div className="containerLogo">
+            {logos.map((logoClass, index) => (
+                <div key={index} className="logo_footer">
+                    <span className={logoClass}></span>
+                </div>
+            ))}
+                </div>
+            <ButtonScrollTop />
         </div>
-    )
+    );
 }

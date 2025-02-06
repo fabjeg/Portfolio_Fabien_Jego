b19d9c3c407aeaa6f4c809aa70bbdbc32bdaa201
Jego <fabien.jego.p@gmail.com>
2025-02-06 17:10:31 +0100

    modify slide

--- src/components/slide/slide.jsx
+++ src/components/slide/slide.jsx
@@ -1,42 +1,41 @@
+import { useState, useEffect, useCallback } from "react";
 import PropTypes from "prop-types";
 import "./style.css";
-export function Slide({ images, currentIndex, setCurrentIndex }) {
-  const nextImage = () => {
-    if (currentIndex < images.length - 1) {
-      setCurrentIndex(currentIndex + 1);
-    } else {
-      setCurrentIndex(0);
-    }
-  };
-  const prevImage = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1);
-    } else {
-      setCurrentIndex(images.length - 1);
-    }
-  };
+
+export function Slide({ images }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const delay = 3000;
+
+  const changeImage = useCallback(
+    (step) => {
+      setCurrentIndex((prev) => (prev + step + images.length) % images.length);
+    },
+    [images.length]
+  );
+  useEffect(() => {
+    const timeout = setTimeout(() => changeImage(1), delay);
+    return () => clearTimeout(timeout);
+  }, [changeImage]);
+
   return (
     <div className="carousel-overlay">
       <div className="carousel-container">
-        <div className="carousel-image carousel-controls">
-          <button onClick={prevImage} className="carousel-arrow">
-            <i className="fa-solid fa-chevron-left"></i>
-          </button>
-          <img
-            src={images[currentIndex]}
-            alt="carousel"
-            className="carousel-img"
-          />
-          <button onClick={nextImage} className="carousel-arrow">
-            <i className="fa-solid fa-chevron-right"></i>
-          </button>
-        </div>
+        <button onClick={() => changeImage(-1)} className="carousel-arrow left">
+          ❮
+        </button>
+        <img
+          src={images[currentIndex]}
+          alt={`Slide ${currentIndex}`}
+          className="carousel-img"
+        />
+        <button onClick={() => changeImage(1)} className="carousel-arrow right">
+          ❯
+        </button>
       </div>
     </div>
   );
 }
+
 Slide.propTypes = {
   images: PropTypes.arrayOf(PropTypes.string).isRequired,
-  currentIndex: PropTypes.number.isRequired,
-  setCurrentIndex: PropTypes.func.isRequired,
 };

9c641e499cfc059a53d6651846ede71f325cce66
Jego <fabien.jego.p@gmail.com>
2025-02-12 09:28:43 +0100

    save

--- src/components/buttons/button-cv.jsx
+++ src/components/buttons/button-cv.jsx
@@ -1,24 +1,5 @@
 import './style.css'
-import ResumePDF from '../../assets/CV.pdf'
-
-export function Button(){
-
-    return (
-<button className="btn-53">
-  {/* <div className="original">Button</div>
-  <div className="letters">
-    
-    <span>M</span>
-    <span>O</span>
-    <span>N</span>
-    <span>&nbsp;</span>
-    <span>C</span>
-    <span>V</span>
-  </div> */}
-</button>
-
-    )
-}
+import ResumePDF from '../../assets/C.V.pdf'
 
 import styled from 'styled-components';
 
